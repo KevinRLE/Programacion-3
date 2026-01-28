47e6a7a4ba8aec347e6462c05385e3109209ac52
Kevin Rodrigo López Espinoza <klopeze14@miumg.edu.gt>
2026-01-28 10:54:42 -0600

    Mi primer programa Hola Mundo

---
+++ miPrimerPrograma/src/main/java/miprimerprograma/MiPrimerPrograma.java
@@ -0,0 +1,18 @@
+/*
+ *Kevin Rorigo Lopez Espinoza
+ *Carnet: 9959-24-3309
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerprograma;
+
+/**
+ *
+ * @author daguilae
+ */
+public class MiPrimerPrograma {
+        public static void main(String[] args) {
+            System.out.println("Hola mundo");
+    }
+}
